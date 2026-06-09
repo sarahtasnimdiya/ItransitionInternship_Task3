94d83ca5c00c8fe601d219c16a4b5e1137a41d7b
Sarah Tasnim Diya <sarahtasnim99@gmail.com>
2026-06-09 12:16:01 +0600

    bug fix- PORT spelling error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 function gcd(a, b) {
   while (b) [a, b] = [b, a % b];
