5524b89c52f46f8fd0e80cccfc86c139021b0a88
Sarah Tasnim Diya <sarahtasnim99@gmail.com>
2026-06-09 11:56:17 +0600

    port fix

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/sarahtasnim99_gmail_com', (req, res) => {
 
     let x = Number(req.query.x);
     let y = Number(req.query.y);
     
-    if(!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
-        return res.type(text).send('NaN');
+    if(!Number.isInteger(x) || !Number.isInteger(y) || x <= 0 || y <= 0) {
+        return res.type('text').send('NaN');
     } 
     
     const gcd = (x, y) => y === 0 ? x : gcd(y, x % y);
@@ -16,4 +16,4 @@ app.get('/sarahtasnim99_gmail_com', (req, res) => {
     res.type('text').send(String(lcm));
 });
 
-app.listen(3000, () => console.log('Server running'));
+app.listen(port, () => console.log('Server running'));
